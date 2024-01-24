6f5d767b6aaccf918d170a3e13536dca9a24e9c6
Davin Moses <davin.moses18@my.benedict.edu>
2024-01-24 17:50:02 -0500

    Added Store

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import './App.css';
+import Store from './Store';
+
+
 
 function App() {
   return (
@@ -6,8 +9,11 @@ function App() {
       <header className="App-header">
         <h5>CodeClicker!</h5>
       </header>
+       <Store/>
     </div>
-  );
+    );
+
+  
 }
 
 export default App;
---
+++ src/Store.js
@@ -0,0 +1,21 @@
+import  Container  from 'react-bootstrap/Container';
+import  Row  from 'react-bootstrap/Row';
+import  Col  from 'react-bootstrap/Col';
+
+function Store(){
+
+    return(
+        <Container>
+            <Row>
+                <Col>1 of 6</Col>
+                <Col>2 of 6</Col>
+                <Col>3 of 6</Col>
+                <Col>4 of 6</Col>
+                <Col>5 of 6</Col>
+                <Col>6 of 6</Col>
+            </Row>
+        </Container>
+    );
+}
+
+export default Store;
