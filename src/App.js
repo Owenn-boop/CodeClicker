401ab23d4a57f0ee5e869abd5361a64a3dfefa3d
Davin Moses <davin.moses18@my.benedict.edu>
2024-01-23 20:36:21 -0500

    Move Header to Top/Resize Complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>CodeClicker!</h1>
+        <h5>CodeClicker!</h5>
       </header>
     </div>
   );
