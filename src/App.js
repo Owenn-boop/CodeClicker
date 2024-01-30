c619ed3e54b099ebbe7641269a81ba2a56373203
Owenn-boop <owennalt@gmail.com>
2024-01-29 19:08:35 -0600

    fixing css not imported

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import './App.css';
+import  Container  from 'react-bootstrap/Container';
+import  Row  from 'react-bootstrap/Row';
+import  Col  from 'react-bootstrap/Col';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h5>CodeClicker!</h5>
-        </header>
+      </header>
 
       <Container>
         <Row>
-          <Col md={4}>
+          <Col>
             {/* Content for the first column */}
             <p>Column 1 Content</p>
           </Col>
-          <Col md={4}>
+          <Col>
             {/* Content for the second column */}
             <p>Column 2 Content</p>
           </Col>
-          <Col md={4}>
+          <Col>
             {/* Content for the third column */}
             <p>Column 3 Content</p>
           </Col>
