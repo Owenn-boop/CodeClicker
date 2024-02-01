345a06f5f98c0533f2c39c2c6714e55e7879d4b6
Owenn-boop <owennalt@gmail.com>
2024-02-01 11:24:32 -0600

    Adding folders for different files

--- src/Components/StoreItem.js
+++ src/Components/StoreItem.js
@@ -1,13 +1,13 @@
 import  Row  from 'react-bootstrap/Row';
 import  Col  from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
-import './StoreItem.css';
+import '../CSS/StoreItem.css';
 import Image from 'react-bootstrap/Image';
-import { getName } from './StoreItemLookup.js';
+import { getName } from '../StoreItemLookup.js';
 
 
 function StoreItem({item}){
-    let item_image = require(`./Assets/${item}.png`);
+    let item_image = require(`../Assets/${item}.png`);
     let item_name = getName(item);
 
     return(
