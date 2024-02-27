6075243b6cc86d128c657204a068ab13a0d47511
Owenn-boop <owennalt@gmail.com>
2024-02-27 01:37:57 -0600

    Prettier change to CodeButton whitespace

--- src/Components/CodeButton.js
+++ src/Components/CodeButton.js
@@ -1,13 +1,20 @@
-import Button from 'react-bootstrap/Button';
-import '../CSS/CodeButton.css'
-import { codeButtonClick } from '../Javascript/codeButtonHelper'
+import Button from "react-bootstrap/Button";
+import "../CSS/CodeButton.css";
+import { codeButtonClick } from "../Javascript/codeButtonHelper";
 
 function CodeButton() {
-  return (
-    <div className='align-self-center'> 
-      <Button className='code-button' onClick={()=>codeButtonClick(document.querySelector('#username').value)}>CodeButton</Button>
-    </div>
-  );
+    return (
+        <div className="align-self-center">
+            <Button
+                className="code-button"
+                onClick={() =>
+                    codeButtonClick(document.querySelector("#username").value)
+                }
+            >
+                CodeButton
+            </Button>
+        </div>
+    );
 }
 
 export default CodeButton;
