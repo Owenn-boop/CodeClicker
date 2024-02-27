01219f823c5b6121b18d9e5c73f831bbebc84ee2
Owenn-boop <owennalt@gmail.com>
2024-02-27 03:02:00 -0600

    Extra = removal from conflict

--- src/Components/CodeButton.js
+++ src/Components/CodeButton.js
@@ -2,9 +2,7 @@ import Button from "react-bootstrap/Button";
 import "../CSS/CodeButton.css";
 import { codeButtonClick } from "../Javascript/codeButtonHelper";
 
-
 function CodeButton({ setCurrentLOC }) {
-=======
     return (
         <div className="align-self-center">
             <Button
